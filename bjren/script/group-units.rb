c308e420238ce04610c1af77dbe2119cfa24eaa3
Ruijian Cao <cao7113@hotmail.com>
2018-10-21 11:13:01 +0800

    fix

--- bjren/script/group-units.rb
+++ bjren/script/group-units.rb
@@ -9,29 +9,32 @@ pats = %w(
   有限责任公司
 )
 commons = %w{
-  阿里 腾讯 华为 百度 万达 三星
-  光大银行 工商银行 民生银行 招商证券
-  ABB 诺基亚 德勤 思科 戴尔 戴姆勒
-  日立 通用电气
-  方正 
-  中交 中信 中治
-  中国电信 中国电力 中国石油
-  中国科学院 中国移动
-  中国航空 中国联合网络通信
-  中国邮政 中国铁路
+  阿里 腾讯 华为 百度 万达 小米
+  三星 亚马逊 戴尔 思科
+  ABB 诺基亚 德勤 戴姆勒
+  日立 通用电气 宝马 苹果 英特尔 西门子 霍尼韦尔
+  光大银行 工商银行 民生银行 北京农村商业
+  招商证券
+  中信 中粮 中治
+  中国电信 中国移动 中国联合网络通信
+  中国电力 中国石油
+  中国科学院 中国航空
+  中国邮政 中国铁路 首钢
   中铁 中青旅
+  方正 同方 联想 神州数码 央视 高德
+  新东方 文思海辉 用友 同仁堂 金山
 }
 
 units.each_with_index do |item, idx|
   name = item[:name]
   dataset = units.where(name: name)
 
-  # step1: filter
+  # step1: remove pattern words
   group0 = name
   group = group0.sub(%r{#{pats.join('|')}}, '') 
   puts "==#{idx}: #{group0} --> #{group}"
 
-  # step2
+  # step2: common big corp
   group0 = group
   hit = commons.find{|i| name.include?(i) }
   if hit
